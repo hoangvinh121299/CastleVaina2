f692e8f7bac2690a4ae142d15e0b4a0d639de257
hoangvinh121299 <42103527+hoangvinh121299@users.noreply.github.com>
2020-09-25 11:22:48 +0700

    Keep install frameword

---
+++ CastleVania/CastleVania/Camera.cpp
@@ -0,0 +1,31 @@
+﻿#include "Camera.h"
+
+
+Camera::Camera(int w, int h)
+{
+	_width = w;
+	_height = h;
+	isAllowFollowSimon = true;
+	isAutoGoX = false;
+
+	//Biên mặc định ban đầu là kích thước map
+	_boundaryLeft = 0;
+	_boundaryRight = 0;
+	_xCamBackup = _yCamBackup = 0;
+	setBoundaryBackup(_boundaryLeft, _boundaryRight);
+	vx = 0;
+}
+Camera::~Camera()
+{
+
+}
+void Camera::Update(DWORD dt)
+{
+	this->dt = dt;
+	if (isAutoGoX == true)
+	{
+		float dx = vx * dt;
+		_xCam += dx;
+	}
+	if ()
+}
--- CastleVania/CastleVania/Camera.h
+++ CastleVania/CastleVania/Camera.h
@@ -1,1 +1,64 @@
-#pragma once
+﻿#ifndef __CAMERA_H__
+#define __CAMERA_H__
+
+#include "define.h"
+
+class Camera
+{
+protected:
+	float _xCam;
+	float _yCam;
+
+	float _xCamBackup;
+	float _yCamBackup;
+	float _boundaryLeftBackup;
+	float _boundaryRightBackup;
+
+	int _width;
+	int _height;
+
+	float _boundaryLeft; //giới hạn camera bên trái
+	float _boundaryRight; // giới hạn camera bên phải
+
+	float vx;
+	DWORD dt;
+
+	bool isAutoGoX; // Có đang ở chế độ tự động hay không
+	float autoGoXDistance; //Khoảng cách cần tự động di chuyển theo trục x
+	float autoGoxBackupX; // Vị trí Camera trước khi tự động di chuyển
+
+	bool isAllowFollowSimon;
+public:
+	Camera(int w, int h);
+	~Camera();
+
+	void Update(DWORD dt);
+
+	D3DXVECTOR2 TransForm(float, float);
+
+	void SetPosition(float x, float y);
+	float GetXCam();
+	float GetYCam();
+	int GetWidth();
+	int GetHeight() :
+
+		bool checkObjectInCamera(float x, float y, float w, float h);
+		
+		bool AllowFollowSimon();
+		void setAllowFollowSimon(bool b);
+
+		void setAutoGoX(float Distance, float Speed);//Tiết lập các thông số auto
+		void stopAutoGoX();
+		void getIsAutoX();
+
+		void SetBoundary(float left, float right);//Set giới hạn biên cho Camera
+		float getBoundaryRight();
+		float getBoundaryLeft();
+
+		void setPositionBackup(float x, float y);
+		void RestorePosition();
+
+		void setBoundaryBackup(float l, float r);
+		void RestoreBoundary();
+};
+#endif
