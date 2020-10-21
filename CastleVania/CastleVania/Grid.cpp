3eb8f8f65cd01d1d65803f286a6acaf0b0a5e92e
Hoàng Vinh <hoangvinh121299@gmail.com>
2020-10-22 01:12:31 +0700

    Install grid

--- CastleVania/CastleVania/Grid.cpp
+++ CastleVania/CastleVania/Grid.cpp
@@ -1,1 +1,49 @@
-#include "Grid.h"
+﻿#include "Grid.h"
+
+Grid::Grid()
+{
+
+}
+Grid::~Grid()
+{
+	//Vòng lặp giải phòng vùng nhớ của Grid
+	for (int i = 0; i < GRID_MAP_CELL_MAX_ROW; i++)
+	{
+		for (int j = 0; j < GRID_MAP_CELL_MAX_COLLUM; j++)
+			mapCells[i][j].clear();
+	}
+}
+void Grid::setObjectFilePath(char* filePath)
+{
+	objectFilePath = filePath;
+}
+void Grid::reloadMapGrid()
+{
+	//Reset giá trị của vector chưa Grid về lại mảng rỗng
+	for (int i = 0; i < GRID_MAP_CELL_MAX_ROW; i++)
+	{
+		for (int j = 0; j < GRID_MAP_CELL_MAX_COLLUM; j++)
+			mapCells[i][j].clear();
+	}
+	
+	float x, y;
+	int objectID, objectType, objectDirection, objectHeight, objectWidth, brickModel, numberofObjects;
+	ifstream fileInfo(objectFilePath, ios::in);
+	fileInfo >> numberofObjects;
+	for (int i=0;i<numberofObjects;i++)
+		for (int i = 0; i < numberofObjects; i++)
+		{
+			
+			fileInfo >> objectID >> objectType >> objectDirection >> x >> y >> objectWidth >> objectHeight >> brickModel;
+			insertObjectToGrid(objectID, objectType, objectDirection, x, y, objectWidth, objectHeight, brickModel);
+		}
+	fileInfo.close();
+}
+GameObject *Grid::createNewGridObject(int objectType, float x, float y, int objectWidth, int objectHeight, int brickModel)
+{
+	switch (objectType)
+	{
+	case objectType::BRICK:
+
+	}
+}
--- CastleVania/CastleVania/Grid.h
+++ CastleVania/CastleVania/Grid.h
@@ -1,11 +1,33 @@
-#ifndef __GRID_H__
+﻿#ifndef __GRID_H__
 #define __GRID_H__
 
 #include "Game.h"
 #include "GameSprite.h"
+#include "define.h"
+#include "GameObject.h"
+#include "Brick.h"
 
+using namespace std;
+#define GRID_CELL_WIDTH (SCREEN_WIDTH /4.0f)
+#define GRID_CELL_HEIGHT (SCREEN_HEIGHT/4.0f)
+
+#define GRID_MAP_CELL_MAX_COLLUM 100//Số lượng hàng tối đa chứa các cell trong map
+#define GRID_MAP_CELL_MAX_ROW 20 //Số lượng hàng tối đa chưa các cell trong map
 class Grid
 {
+private:
+	vector<GameObject*> mapCells[GRID_MAP_CELL_MAX_ROW][GRID_MAP_CELL_MAX_COLLUM]; // Ma trận chứa các cell trong map
+	char *objectFilePath;
+public:
+	Grid();
+	~Grid();
+
+	void setObjectFilePath(char*objectFilePath);
+	void reloadMapGrid();
+
+	GameObject * createNewGridObject(int objectType, float x, float y, float objectWidth, int objectHeight, int brickModel);
+	void insertObjectToGrid(int objectID, int ObjectTye, int objectDirection, float x, float y, int objectWidth, int objectHeight, int brickModel);
+	void getListObjectFromMapGrid(vector<GameObject*> objectsList, Camera *camera);
 };
 
 #endif
